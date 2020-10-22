b58dff3746aec97deab09e9d62b649c5141b81d4
Loïc <monos-oniromancie@live.fr>
2020-10-22 06:51:18 +0200

    Compatible avec la version 0.1.0.0 et supérieur

--- projets/charset_exemple/source/main.c
+++ projets/charset_exemple/source/main.c
@@ -3,8 +3,8 @@
 *******************************************************
 * Role ........... : exemple pour poser un charset    *
 * Auteur ......... : Jean Monos                       *
-* Version ........ : V 1.0.0.0                        *
-* Modification ... : 01/03/2020                       *
+* Version ........ : V 2.0.0.0                        *
+* Modification ... : 22/10/2020                       *
 * Licence ........ : Creative Commons by-sa           *
 * Compilateur .... : cc65                             *
 *******************************************************/
@@ -33,9 +33,9 @@ void main()
   SCREEN_OFF;    // L'écran est désactivé.
   set_loram_ram(); // Desactivation de la Rom Basic. 
   set_location_character(2); // La lecture des paterns se fait à l'adresse $0800
-  load_pattern(0x0800,data_tiles,2); // Mise en mémoire des tiles à l'adresse $0800
+  set_data_character(0x0800,data_tiles,2); // Mise en mémoire des tiles à l'adresse $0800
   cls(0); // On efface l'écran avec le patern numero 0 des tiles.
-  draw_full_charset(10,12,1,5); // On affiche le tile 1, à la position 10-12 avec la couleur numero 5.
+  draw_full_character(10,12,1,5); // On affiche le tile 1, à la position 10-12 avec la couleur numero 5.
   wait_vbl(); // on attend le début du Vblank.
   SCREEN_ON; // On active l'écran.
   
