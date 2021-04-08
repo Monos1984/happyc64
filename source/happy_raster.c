6304e9df0086fd92506d411f9bfc2fea21ef42eb
Loïc <monos-oniromancie@live.fr>
2021-04-08 07:01:53 +0200

    0.2.2.0

--- source/happy_raster.c
+++ source/happy_raster.c
@@ -13,18 +13,14 @@
 // ====================
 #include <happyc64.h>
 
-  
-  // =====================
-  // * init interruption *
-  // =====================
-  void init_adr_irq(unsigned int adresse)
-  {
-  
-  
+// =====================
+// * init interruption *
+// =====================
+void init_adr_irq(unsigned int adresse)
+{    
   POKE(REG_ADR_HAUT_IRQ,(adresse & 0b1111111100000000)>>8);
-  POKE(REG_ADR_BASS_IRQ,(adresse & 0b0000000011111111)   );
-  
-  }
+  POKE(REG_ADR_BASS_IRQ,(adresse & 0b0000000011111111)   );  
+}
   
 // ================================
 // * Recuperer la ligne du raster *
@@ -32,6 +28,7 @@
 unsigned int get_raster(void)
 {
   unsigned int raster;
+  
   raster = (PEEK(0xD011)&1)<<8+PEEK(0xD012);
   return raster;     
 }
