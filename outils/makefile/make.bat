b290a5e8d3071369337381dfb0ea16bfdf35863b
Loïc <monos-oniromancie@live.fr>
2021-04-10 09:09:20 +0200

    Nouveau makefile

--- outils/makefile/make.bat
+++ outils/makefile/make.bat
@@ -1,3 +1,22 @@
 @echo off
+
+rem -------------------------------
+rem - Lancement de la compilation -
+rem -------------------------------
+
+
+rem  ..\..\outils\makefile\make 
+rem  fichier prg
+
+rem ..\..\outils\makefile\make floppy
+rem fichier d64
+
+rem ..\..\outils\makefile\make tap
+rem fichier tap
+
+rem ..\..\outils\makefile\make floptap
+rem fichier d64 et tap
+
+
 ..\..\outils\makefile\make 
 pause
